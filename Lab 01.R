c98698069402d5dd9c00858b4e6d0e3dc9902f7b
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-04-21 17:12:55 +0530

    Input something on Lab 01 file

---
+++ Lab 01.R
@@ -0,0 +1,19 @@
+#Check working directly
+getwd()
+
+#Change and check working directly
+setwd("C:\\Users\\HP\\OneDrive\\Desktop\\New folder\\R")
+#OR
+setwd("C:/Users/HP/OneDrive/Desktop/New folder/R")
+getwd()
+
+#help
+?solve
+help("data.frame")
+
+#Install packages
+install.packages("ggplot2")
+
+#Scale Operators
+#add
+2+8
